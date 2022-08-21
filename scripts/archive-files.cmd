34522ed0d37a7aebb31dd499925c0e1c915647ab
0xCM00 <0xCM00@gmail.com>
2022-08-20 22:04:38 -0500

    .

--- scripts/archive-files.cmd
+++ scripts/archive-files.cmd
@@ -3,6 +3,6 @@ call %~dp0..\config.cmd
 set Src=%SlnRoot%
 set Dst=%EnvRoot%\archives\dev\%SlnId%
 set XD=/xd %Src%/.git /xd %Src%/.vs /xd %Src%artifacts /xd %Src%/.vscode /xd %Src%/deps
-set CmdSpec=robocopy %Src% %Dst% /e %XD%
-echo ArchiveFiles:%CmdSpec%
-: call %CmdSpec%
+set ArchiveFiles=robocopy %Src% %Dst% /e %XD%
+echo ArchiveFiles:%ArchiveFiles%
+call %ArchiveFiles%
