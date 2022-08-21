799c59fbfab41e80112acea024222c731da63f0a
0xCM00 <0xCM00@gmail.com>
2022-08-20 22:12:09 -0500

    .

--- scripts/archive-files.cmd
+++ scripts/archive-files.cmd
@@ -1,8 +1,8 @@
 @echo off
 call %~dp0..\config.cmd
-set Src=%SlnRoot%
-set Dst=%EnvRoot%/archives/dev/%SlnId%
-set XD=/xd %Src%/.git
-set ArchiveFiles=robocopy %Src% %Dst% %XD% /mir
+set Src=%EnvRoot%\dev\z0
+set Dst=%EnvRoot%\archives\dev\%SlnId%
+set XD=/xd %EnvRoot%\dev\%SlnId%\.git
+set ArchiveFiles=robocopy %Src% %Dst% %XD% /e
 echo ArchiveFiles:%ArchiveFiles%
 call %ArchiveFiles%
