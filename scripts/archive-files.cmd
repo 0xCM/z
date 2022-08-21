def6f003c16dbd21654b9be84c8790c1c8c1ea37
0xCM00 <0xCM00@gmail.com>
2022-08-20 22:05:34 -0500

    .

--- scripts/archive-files.cmd
+++ scripts/archive-files.cmd
@@ -2,7 +2,7 @@
 call %~dp0..\config.cmd
 set Src=%SlnRoot%
 set Dst=%EnvRoot%\archives\dev\%SlnId%
-set XD=/xd %Src%/.git /xd %Src%/.vs /xd %Src%artifacts /xd %Src%/.vscode /xd %Src%/deps
+set XD=/xd %Src%/.git /xd %Src%/deps
 set ArchiveFiles=robocopy %Src% %Dst% /e %XD%
 echo ArchiveFiles:%ArchiveFiles%
 call %ArchiveFiles%
