f88d32ae5c1ef405a9bc78d7886401fdd4a3f51b
John Doe <you@example.com>
2023-03-17 21:29:32 -0500

    .

---
+++ src/projects/Part.cs
@@ -0,0 +1,11 @@
+//-----------------------------------------------------------------------------
+// Copyright   :  (c) Chris Moore, 2020
+// License     :  MIT
+//-----------------------------------------------------------------------------
+[assembly: PartId("projects")]
+namespace Z0.Parts
+{
+    public sealed class Projects : Part<Projects>
+    {
+    }
+}
---
+++ src/projects/config.cmd
@@ -0,0 +1,3 @@
+@echo off
+set ProjectName=projects
+call %~dp0..\config.cmd
