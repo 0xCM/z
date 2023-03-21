c55a4b54fa074d6d3087cd4f665e172919665f8c
John Doe <you@example.com>
2023-03-21 08:47:58 -0500

    .

--- cmd/deploy.cmd
+++ cmd/deploy.cmd
@@ -3,7 +3,7 @@ set ProjectName=cmd
 call %~dp0..\config.cmd
 set DeployPath=%DevTools%\%BuildPrefix%\bin
 set DeployLog=-bl:%BuildLogs%\%BuildPrefix%.%ProjectName%.deploy.binlog
-set DeployProps=-p:PublishReadyToRun=true -p:DebugType=pdbonly -p:PublishDocumentationFiles=true -p:CopyLocalLockFileAssemblies=true -p:CopyDebugSymbolsFromPackages=true -p:CopyDocumentationFilesFromPackages=true
 set DeployProps=-p:PublishReadyToRun=true -p:DebugType=pdbonly
+set DeployProps=-p:PublishReadyToRun=true -p:DebugType=pdbonly -p:PublishDocumentationFiles=true -p:CopyDebugSymbolsFromPackages=true -p:CopyDocumentationFilesFromPackages=true
 set DeployApp=dotnet publish %ProjectPath% --output %DeployPath% %ConfigOption% %VerbosityOption% %FrameworkOption% %DeployProps% %DeployLog%
 call %DeployApp%
